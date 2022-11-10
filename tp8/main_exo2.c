d8169e45f64e5b0850ba3e54fed3ff93275d3009
Pierre Brognaux <pierre.brognaux@edu.ece.fr>
2022-11-10 12:19:07 +0100

    pousse le fichier test

---
+++ tp8/main_exo2.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int nbrNotes = 0;
+    int note = 0;
+    printf("ecrivez vos notes, si vous voulez arreter ecrivez stop.");
+    while(0 <= note && note <= 20){
+        scanf("%d",&note);
+
+    }
+}
+
+void procedure(int entier)
